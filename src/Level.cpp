78fc95cc941901f3d83ce75739042b1e940c9130
Chris Moylan <chris@chrismoylan.com>
2016-12-23 10:55:33 -0600

    switch level debug to cout

--- src/Level.cpp
+++ src/Level.cpp
@@ -7,16 +7,16 @@ AssetList Level::assetData()
     for (auto tilesetPair : this->tilesets) {
         // load into an asset, append to asset list
         Tileset& tileset = tilesetPair.second;
-        
+
         Asset asset = {};
         asset.spriteFilename = tileset.filename;
         asset.width = tileset.width;
         asset.height = tileset.height;
         asset.name = tileset.filename;
-        
+
         assets.push_back(asset);
     };
-    
+
     return assets;
 }
 
@@ -24,42 +24,52 @@ AssetList Level::assetData()
 void
 Level::printPlatforms()
 {
-    // FIXME: use IOSTREAM
-    using namespace std;
-    cout << "this work?" << endl;
     // ----- print out the level ----- //
+    using namespace std;
+
     Layer& layer = layers.find("platforms")->second;
-    std::vector<int>::iterator p;
     int i, row;
     row = 0;
 
-    printf("\n[%d]:  ", row);
-    for (p = layer.tiles.begin(), i = 1; p != layer.tiles.end(); ++p, ++i) {
-        printf("%d | ", *p);
+    cout << endl << "[" << row << "]: ";
+    for (auto tile : layer.tiles) {
+        cout << tile << " | ";
         if ((i % mapWidth == 0) && (row < (mapHeight - 1))) {
             ++row;
-            printf("\n[%d]:  ", row);
+	    cout << endl << "[" << row << "]:";
         }
+	i++;
     }
-    printf("\n");
-    printf("tile count is: %d\n", layer.tileCount);
+    cout << endl << "tile count is: " << layer.tileCount << endl;
 }
 
 
-RenderMap 
+RenderMap
 Level::renderData()
 {
     RenderMap map;
     // for each asset
     // for each square to render
     // assume 1:1 LayerMap to Tileset
-    
+
     for (auto& layerPair : layers) {
         // only do platforms for now...remove this later
         if (layerPair.first == "platforms") {
-            layerPair.second;
+            // assume only one asset for now
+            auto& layer = layerPair.second;
+            size_t i = 0; // TODO: better way to do this?
+            for (auto tile : layer.tiles) {
+              // make a rect for each of these
+              // then use the GID to use the right sprite
+              Rectangle rect = {};
+              rect.x = i % layer.width;
+              rect.y = i % layer.height;
+              //rect.clipX;
+              //rect.clipY;
+              i++;
+            }
         }
     };
-    
+
     return map;
 }
